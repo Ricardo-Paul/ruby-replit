b06cd30103e46977ae9f703c785e91eb746ee2ee
RicardoPaul <>
2022-08-24 03:08:13 +0000

    decouple the subject with the observers

--- Design Patterns/observer.rb
+++ Design Patterns/observer.rb
@@ -1,25 +1,60 @@
 class Employee
   attr_reader :salary, :name
-  def initialize(name, title, salary, payroll)
+  def initialize(name, title, salary)
     @name = name
     @title = title
     @salary = salary
-    @payroll = payroll
+    @observers = []
   end
 
+  def add_observers(observers)
+    observers.each do |observer|
+      add_observer(observer)
+    end
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  def remove_observer(observer)
+    @observers.delete(observer)
+  end
+
+  # We are writing the setter by hand because we want to inform the payroll
   def salary=(new_salary)
     @salary = new_salary
-    @payroll.update( self )
+    notify_observers
+  end
+
+  def notify_observers
+    @observers.each do |observer|
+      observer.update( self )
+    end
   end
 end
 
 class Payroll
   def update(employee)
+    puts "FROM PAYROLL"
     puts "Employee #{employee.name} new salary is #{employee.salary}"
   end
 end
 
+class TaxMan
+  def update(employee)
+    puts "FROM TAXMAN"
+    puts "Employee #{employee.name} new salary is #{employee.salary}"
+  end
+end
+
+# observers, interested in jim salary changes
 payroll = Payroll.new
-employee = Employee.new('Russ', 'Doe', 1500, payroll)
-puts(employee.inspect)
-employee.salary=3500
+taxman = TaxMan.new
+
+# Jim, the employee
+jim = Employee.new('Russ', 'Doe', 1500)
+
+jim.add_observers([payroll, taxman])
+
+jim.salary=3500
