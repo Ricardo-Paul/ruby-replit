b7ee7e410d0ca011c3485f2577953aa5914a85e2
Ricardo-Paul <ricardopaul459@gmail.com>
2022-08-25 01:40:28 +0000

    switch to proc based observers

--- Design Patterns/observer.rb
+++ Design Patterns/observer.rb
@@ -1,9 +1,5 @@
-class Employee
-  attr_reader :salary, :name
-  def initialize(name, title, salary)
-    @name = name
-    @title = title
-    @salary = salary
+class Subject
+  def initialize
     @observers = []
   end
 
@@ -21,25 +17,35 @@ class Employee
     @observers.delete(observer)
   end
 
+  def notify_observers
+    @observers.each do |observer|
+      observer.call( self )
+    end
+  end
+end
+
+class Employee < Subject
+  attr_reader :salary, :name
+  def initialize(name, title, salary)
+    super()
+    @name = name
+    @title = title
+    @salary = salary
+  end
+
   # We are writing the setter by hand because we want to inform the payroll
   def salary=(new_salary)
     @salary = new_salary
     notify_observers
   end
-
-  def notify_observers
-    @observers.each do |observer|
-      observer.update( self )
-    end
-  end
 end
 
-class Payroll
-  def update(employee)
-    puts "FROM PAYROLL"
-    puts "Employee #{employee.name} new salary is #{employee.salary}"
-  end
-end
+# class Payroll
+#   def update(employee)
+#     puts "FROM PAYROLL"
+#     puts "Employee #{employee.name} new salary is #{employee.salary}"
+#   end
+# end
 
 class TaxMan
   def update(employee)
@@ -49,12 +55,23 @@ class TaxMan
 end
 
 # observers, interested in jim salary changes
-payroll = Payroll.new
-taxman = TaxMan.new
+# payroll = Payroll.new
+# taxman = TaxMan.new
+
+# Using ruby code blocks instead of classes
+PAYROl_OBSERVER = lambda do |employee|
+  puts "FROM PAYROLL"
+  puts "Employee #{employee.name} new salary is #{employee.salary}"
+end
+
+TAXMAN_OBSERVER = lambda do |employee|
+  puts "FROM TAXMAN"
+  puts "Employee #{employee.name} new salary is #{employee.salary}"
+end
 
 # Jim, the employee
 jim = Employee.new('Russ', 'Doe', 1500)
 
-jim.add_observers([payroll, taxman])
+jim.add_observers([PAYROl_OBSERVER, TAXMAN_OBSERVER])
 
 jim.salary=3500
