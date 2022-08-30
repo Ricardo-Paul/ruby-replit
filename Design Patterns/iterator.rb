bb152e7b312dd322503a51aa704be71262d3f324
RicardoPaul <>
2022-08-30 02:47:00 +0000

    take advantage of ruby enumerable in aggregate class

--- Design Patterns/iterator.rb
+++ Design Patterns/iterator.rb
@@ -20,8 +20,51 @@ class ArrayIterator
 end
 
 patterns = ['composite', 'iterator', 'observer']
-iterator = ArrayIterator.new(patterns)
+iterator = ArrayIterator.new('Ricardo')
 
 while iterator.has_next?
   puts iterator.next_item
 end
+
+# Internal Iterator
+def each_element(array)
+	i = 0
+	while i < array.length
+		yield(array[i])
+		i += 1
+	end
+end
+patterns = ['Template Method', 'Strategy', 'Observer']
+each_element(patterns) {|p| puts(p)}
+
+class Portfolio
+  include Enumerable
+  attr_reader :accounts
+  def initialize
+    @accounts = []
+  end
+  def each(&block)
+    @accounts.each(&block)
+  end
+  def add_account(account)
+    @accounts << account
+  end
+end
+
+class Account
+  attr_reader :name, :balance
+  def initialize(name, balance)
+    @name = name
+    @balance = balance
+  end
+  def <=>(other)
+    balance <=> other.balance
+  end
+end
+
+portfolio = Portfolio.new
+jane_doe = Account.new('Jane', 2000)
+portfolio.add_account(jane_doe)
+
+accounts = portfolio.all?{|acc| acc.balance > 100}
+puts accounts
