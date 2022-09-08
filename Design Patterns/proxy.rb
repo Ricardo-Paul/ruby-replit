b413decf58fe154b9a58cd5dc93b3e6bbd1beb05
Ricardo-Paul <ricardopaul459@gmail.com>
2022-09-08 02:07:31 +0000

    a protection proxy

--- Design Patterns/proxy.rb
+++ Design Patterns/proxy.rb
@@ -1,3 +1,5 @@
+require 'etc'
+
 class BankAccount
   attr_reader :balance
 	def initialize(starting_bal = 0)
@@ -9,20 +11,36 @@ class BankAccount
 end
 
 class BankAccountProxy
-	def initialize(real_object)
+	def initialize(real_object, username)
 		@real_object = real_object
+    @username = username
 	end
+  
 	def deposit(amount)
+    check_access("deposit")
 		@real_object.deposit(amount)
 	end
+  
   def balance
+    check_access("balance check")
     @real_object.balance
+  end
+  
+  def check_access(action)
+    # unless Etc.getlogin == @username raise "Error"
+    if Etc.getlogin != @username
+      raise "Permission error: cannot perform #{action}"
+    end
   end
 end
 
+# client code
 acc = BankAccount.new
 acc.deposit(8000)
 puts acc.balance
 
-accProxy = BankAccountProxy.new(acc)
+accProxy = BankAccountProxy.new(acc, "runner")
+accProxy.deposit(1200)
 puts accProxy.balance
+
+puts Etc.getlogin
