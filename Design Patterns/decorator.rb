388656b11010ae6e791dd31a2c4f4ab5a9f50b3b
RicardoPaul <>
2022-09-10 20:57:53 +0000

    a decorator for simple writer

--- Design Patterns/decorator.rb
+++ Design Patterns/decorator.rb
@@ -1,22 +1,39 @@
-class EnhancedWriter
+class SimpleWriter
+  attr_reader :file
 	def initialize(path)
 		@file = File.open(path, 'w')
 	end
 	def write_line(line)
 		@file.print(line + "\n")
 	end
-	def timestamping_write_line(text)
-		write_line("#{Time.new}: #{text}")
-	end
-	# and so on
+	# def timestamping_write_line(text)
+	# 	write_line("#{Time.new}: #{text}")
+	# end
 	def close
 		@file.close
 	end
 end
 
-# Now we can use our enhanced writer to write the way we want
-writer = EnhancedWriter.new('./Design Patterns/textfile.txt')
-writer.write_line('Plain text')
+class NumberingWriter
+  def initialize(real_writer)
+    @real_writer = real_writer
+    @line_number = 1
+  end
 
-# OR
-writer.timestamping_write_line('Line with timestamp')
+  def write_line(line)
+    @real_writer.write_line("#{@line_number} #{line}")
+    log
+    @line_number += 1
+  end
+
+  def log
+    puts "#{@real_writer.file.path} has been modified"
+  end
+end
+
+# Now we can use our enhanced writer to write the way we want
+simple_writer = SimpleWriter.new('./Design Patterns/textfile.txt')
+numbering_writer = NumberingWriter.new(simple_writer)
+
+numbering_writer.write_line("Adapter Pattern")
+numbering_writer.write_line("Command Pattern")
