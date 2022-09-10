59461e994bf2072fdf8e866b7bdf1d4bac1ad4e9
RicardoPaul <>
2022-09-10 19:28:52 +0000

    better comments - rename

--- tax_calculator.rb
+++ tax_calculator.rb
@@ -4,12 +4,12 @@ class TaxCalculator
    end
 
   def calculate_tax(amount)
-    # the bock provided to the class is where the calculation happens
-    "#{@name} is #{@block.call(amount)}"
+    # closure
+    "#{@name} is #{@block.call(amount)} USD"
   end
 end
 
-puts "=== working with TaxCalculator ==="
-tx = TaxCalculator.new("Sales Tax") {|amount| amount * 5}
+puts "=== TaxCalculator ==="
+sales_tax_calc = TaxCalculator.new("Sales Tax") {|p| p * 5}
 
-puts tx.calculate_tax(5)
+puts sales_tax_calc.calculate_tax(4)
