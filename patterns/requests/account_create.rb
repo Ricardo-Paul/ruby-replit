ed1f4413c8f8a59ca83275dcb03d5161e1304a1e
Ricardo-Paul <ricardopaul459@gmail.com>
2022-09-10 19:14:49 +0000

    clean up

--- patterns/requests/account_create.rb
+++ patterns/requests/account_create.rb
@@ -1,11 +1,9 @@
 require_relative '../request'
 
 class AccountCreate < Reepay::Request
-  # why do we even need the define_attribute method here
   define_attribute 'company', String
 end
 
-# now we are able to initialize
 account = AccountCreate.new('Google')
 account.company = "Apple Co"
 puts "the company: #{account.company}"
